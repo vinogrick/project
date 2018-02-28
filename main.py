5c0a5c29606668d15787e940dfb1518c2819742a
vinogrick <36858862+vinogrick@users.noreply.github.com>
2018-02-28 17:47:08 +0000

    Add files via upload
    Левая сторона экрана - огромный джойстик (нормальный джойстик).

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
 
 class Player(Widget):
@@ -11,7 +12,6 @@ class Player(Widget):
 
 class ShooterGame(Widget):
     player = ObjectProperty(None)
-    
     
     def on_touch_down(self, touch):
         global x0, y0
@@ -23,16 +23,41 @@ class ShooterGame(Widget):
     
     
     def on_touch_move(self, touch):
-        x = touch.x
-        y = touch.y
-        vector = (x - x0, y - y0)
-        self.player.move(vector)
-    pass
+        global x, y
+        if touch.x < self.width//2:
+            x = touch.x
+            y = touch.y
+        else:
+            pass
+        
+        
+    def update(self, dt):
+        try:
+            global x, y, x0, y0
+            if x0 != x or y0 != y:
+                r = 2
+                R = ((x - x0)**2 + (y - y0)**2)**0.5
+                try:
+                    x_itog = (x - x0) * r/R
+                    y_itog = (y - y0) * r/R
+                    vector = (x_itog, y_itog)
+                    self.player.move(vector)
+                except Exception:
+                    None
+        except Exception:
+            None
+    
+    
+    def on_touch_up(self, touch):
+        global x0, y0, y, x
+        x = x0
+        y = y0
 
 
 class ShooterApp(App):
     def build(self):
         game = ShooterGame()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
 
 
